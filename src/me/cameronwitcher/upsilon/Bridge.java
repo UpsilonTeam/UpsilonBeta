25fdebfcd410e3049cbcc97bf368a48378dcebb5
Cameron Witcher <Cameron Witcher@HS-BUS-27.wellsville-usd289.org>
2015-12-16 12:25:45 -0600

    UPDATE

--- src/me/cameronwitcher/upsilon/Bridge.java
+++ src/me/cameronwitcher/upsilon/Bridge.java
@@ -1,17 +1,18 @@
 package me.cameronwitcher.upsilon;
 
 import java.awt.Dimension;
-
 import java.io.File;
 import java.io.StringReader;
 
 import javax.swing.JFrame;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Endpoint;
 import javax.xml.ws.Provider;
 import javax.xml.ws.Service;
 import javax.xml.ws.ServiceMode;
 import javax.xml.ws.WebServiceProvider;
+import javax.xml.ws.http.HTTPBinding;
 
 import me.cameronwitcher.upsilon.boards.Game;
 import me.cameronwitcher.upsilon.boards.GameBoard;
@@ -30,6 +31,7 @@ public class Bridge implements Provider<Source> {
 	public Source invoke(Source request) {
         return  new StreamSource(new StringReader("<p>Hello There!</p>"));
     }
+	
 
 	public static void main(String[] args) {
 		game = new Game();
@@ -39,6 +41,14 @@ public class Bridge implements Provider<Source> {
 			Utils.runInstall();
 		}
 		Utils.init();
+		
+		String address = "http://127.0.0.1:8080/";
+        Endpoint.create(HTTPBinding.HTTP_BINDING, new Bridge()).publish(address);
+
+        System.out.println("Service running at " + address);
+        System.out.println("Type [CTRL]+[C] to quit!");
+
+        Thread.sleep(Long.MAX_VALUE);
 
 	}
 	
