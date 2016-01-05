275e2f9f9775a1a173a2326db29d6e2e9dc95d33
Cameron Witcher <CameronWitcher2017@gmail.com>
2016-01-05 10:08:38 -0600

    CHANGE: Death messages
    Changed one of the death messages when you are shot.

--- src/me/cameronwitcher/upsilon/utils/DamageReason.java
+++ src/me/cameronwitcher/upsilon/utils/DamageReason.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public enum DamageReason {
 	VOID("You fell out of the world!:You're a clumsy one, aren't you?"),
 	ENEMY("You were killed by an enemy.:They got you.. Try again? I bet you can get 'em.:Ouch. Beat to death. I say get REVENGE! >=D"),
-	PROJECTILE("You were shot!:You took an arrow to the knee!"),
+	PROJECTILE("You were shot!:You took an arrow to the knee! I think.."),
 	BAD_THINGS("You probably stepped on a spike. Painful!:Ouch. Looks like you stepped on something painful!");
 	
 	String reason;
